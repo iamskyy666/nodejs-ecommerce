d59c5897f91db99d31eb403160819f130eee2d2a
Soumadip <39thdecember@gmail.com>
2026-06-30 22:37:04 +0530

    Setup basic routes ☑️

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./db/connect.js";
+import notFound from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler";
 
 const app = express();
+
+// express middlewares
+app.use(express.json());
 
 app.get("/", (_, res) => {
   res.send("ECommerce API running... ✅");
 });
 
+// custom middlewares
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 const PORT = process.env.PORT || 5000;
 
+// start server
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
