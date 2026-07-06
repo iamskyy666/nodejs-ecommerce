d5d316bde6922024422df824a2a1ae246a112149
Soumadip <39thdecember@gmail.com>
2026-07-06 15:03:02 +0530

    Add all CRUD handlers regarding Product, apart from uploadProductImage( ) 🟢

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import Product from "../models/product.model.js";
+import NotFoundError from "../errors/not-found.js";
 
 async function createProduct(req, res) {
   req.body.user = req.user.userId;
@@ -7,22 +8,51 @@ async function createProduct(req, res) {
   res.status(StatusCodes.CREATED).json({ created_product: product });
 }
 
-async function getAllProducts(req, res) {
-  res.send("getAllProducts");
+async function getAllProducts(_, res) {
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({
+    msg: `✅ Fetched All ${products.length} Products Successfully!`,
+    all_products: products,
+  });
 }
 
 async function getSingleProduct(req, res) {
-  res.send("getSingleProduct");
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    throw new NotFoundError("🔴 Product not found!");
+  }
+  res.status(StatusCodes.OK).json({
+    product,
+  });
 }
 
 async function updateProduct(req, res) {
-  res.send("updateProduct");
+  const product = await Product.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true, runValidators: true },
+  );
+  if (!product) {
+    throw new NotFoundError("🔴 Product not found!");
+  }
+  res.status(StatusCodes.OK).json({
+    updated_product: product,
+  });
 }
 
 async function deleteProduct(req, res) {
-  res.send("deleteProduct");
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (!product) {
+    throw new NotFoundError("🔴 Product not found!");
+  }
+
+  res.status(StatusCodes.OK).json({
+    msg: "🟢 Product removed successfully!",
+  });
 }
 
+// later
 async function uploadProductImage(req, res) {
   res.send("uploadProductImage");
 }
