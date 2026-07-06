34f26bf62ca849c8a87c741d4bd0b47d0a775c0a
Soumadip <39thdecember@gmail.com>
2026-07-06 22:21:34 +0530

    Add createReview( ) controller ☑️

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,11 +3,38 @@
 // Handles review management operations.
 // ============================================================================
 
+import { StatusCodes } from "http-status-codes";
+import Review from "../models/review.model.js";
+import Product from "../models/product.model.js";
+import NotFoundError from "../errors/not-found.js";
+import BadRequestError from "../errors/bad-request.js";
+
 // @desc    Create a new review
 // @route   POST /api/v1/reviews
 // @access  Private
 async function createReview(req, res) {
-  res.send("createReview()");
+  const { product: productId } = req.body;
+  const isValidProduct = await Product.findById(productId);
+
+  // Check for non-existent product.
+  if (!isValidProduct) {
+    throw new NotFoundError(`🔴 No product with ID: ${productId}`);
+  }
+
+  // Check is for already submitted review for the product.
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmitted) {
+    throw new BadRequestError(`🔴 Already submitted review for this product!`);
+  }
+
+  // Finally, submit review
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ created_review: review });
 }
 
 // @desc    Get all reviews
